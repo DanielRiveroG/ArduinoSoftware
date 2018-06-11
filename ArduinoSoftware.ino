d38b082bfcb65207c3d45a0571d971ef9df82540
Unknown <danielriveroguerra@hotmail.com>
2018-06-11 22:53:55 +0100

    Instructions for variables added.

--- ArduinoSoftware.ino
+++ ArduinoSoftware.ino
@@ -4,23 +4,40 @@ byte commandSize = 0;
 byte commandRead = 0;
 uint16_t digitalOutputPins[] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37};
 uint16_t digitalInputPins[] = {42,43,44,45,46,47,48,49};
+uint16_t analogInputPins[] = {};
 
 void outputBit(){
-  digitalWrite(digitalOutputPins[splitCommand[1].toInt()], splitCommand[2].toInt());  
+  digitalWrite(digitalOutputPins[splitCommand[1].toInt()], splitCommand[2].toInt());
+  Serial.println("!AK");  
 }
 
-byte inputBit(){
-  return digitalRead(digitalInputPins[splitCommand[1].toInt()]);
+void inputBit(){
+  byte tmp = digitalRead(digitalInputPins[splitCommand[1].toInt()]);
+  Serial.println("!AK");
+  (tmp == 0)? Serial.println("!OI 0"):Serial.println("!OI 1");
 }
 
 void outputToggle(){
-  digitalWrite(digitalOutputPins[splitCommand[1].toInt()], !digitalRead(digitalOutputPins[splitCommand[1].toInt()]));  
+  digitalWrite(digitalOutputPins[splitCommand[1].toInt()], !digitalRead(digitalOutputPins[splitCommand[1].toInt()]));
+  Serial.println("!AK");  
 }
 
 void outputPulse(){
   digitalWrite(digitalOutputPins[splitCommand[1].toInt()], splitCommand[2].toInt());
   delay(splitCommand[3].toInt());  
   digitalWrite(digitalOutputPins[splitCommand[1].toInt()], !splitCommand[2].toInt());
+}
+
+void inputAnalog(){
+  int result = analogRead(analogInputPins[splitCommand[1].toInt()]);
+  Serial.println("!AK");
+  Serial.println("!OI " + String(result));
+}
+
+void inputByte(){
+  int result = PINL;
+  Serial.println("!AK");
+  Serial.println("!OI " + String(result));
 }
 
 void setup() {
@@ -38,16 +55,18 @@ void executeCommand(){
   }
   if(splitCommand[0].equals("OB")){
     outputBit();
-    Serial.println("!AK");
   }
   if(splitCommand[0].equals("OT")){
     outputToggle();
-    Serial.println("!AK");
   }
   if(splitCommand[0].equals("IB")){
-    byte tmp = inputBit();
-    Serial.println("!AK");
-    (tmp == 0)? Serial.println("!OI 0"):Serial.println("!OI 1");
+    inputBit();
+  }
+  if(splitCommand[0].equals("AI")){
+    inputAnalog();
+  }
+  if(splitCommand[0].equals("IL")){
+    inputByte();
   }
   if(splitCommand[0].equals("OP")){
     outputPulse();
